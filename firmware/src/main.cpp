7aba1c52737fbecc6679f853d239b323a7ddb41b
Vit Faltus <faltuvit@cvut.cz>
2026-05-07 21:04:26 +0200

    Firmware: add audio settings to main

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
-#include"input_hander.hpp"
+#include "input_hander.hpp"
+#include "audio_settings.hpp"
 
 InputHandler* input_handler;
-
+AudioSettings* audio_settings;
 
 void setup() {
+  Serial.begin(9600);
+
   input_handler = new InputHandler();
+  audio_settings = new AudioSettings();
 
 }
 
@@ -14,8 +18,8 @@ void loop() {
   InputHandler::button_press button_input = input_handler->button_check();
 
   switch (button_input){
-    case InputHandler::left_button_press:
-      // volume down
+    case InputHandler::left_button_press: // volume down
+      audio_settings->volume_down();
       break;
     case InputHandler::left_button_long_press:
       // previous song
@@ -23,8 +27,8 @@ void loop() {
     case InputHandler::middle_button_press:
       // pause/play song
       break;
-    case InputHandler::right_button_press:
-      // volume up
+    case InputHandler::right_button_press: // volume up
+      audio_settings->volume_up();
       break;
     case InputHandler::right_button_long_press:
       // next song
