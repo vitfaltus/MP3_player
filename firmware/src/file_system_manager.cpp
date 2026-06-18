b8efa3d719864156f3c5c3b80320c6b45bcdc55d
Vit Faltus <faltuvit@cvut.cz>
2026-06-18 18:42:53 +0200

    Firmware: finished implementing filesys manager

--- firmware/src/file_system_manager.cpp
+++ firmware/src/file_system_manager.cpp
@@ -6,8 +6,13 @@
 #include <SPI.h>
 
 #define VOLUME_STRING  "Volume: "
+#define SETTINGS_PATH  "/system/settings.txt"
+
 #define PLAYLIST_ROOT_STRING "Playlist root: "
+#define PLAYLIST_PATH "/system/playlist.txt"
+
 #define CURRENT_PLAYING_STRING "Current song: "
+#define SONG_PATH "/system/song.txt"
 
 
 void FileSystemManager::create_system_folder() {
@@ -16,7 +21,7 @@ void FileSystemManager::create_system_folder() {
   create_playlist_file();
 }
 void FileSystemManager::create_settings_file() {
-  File f = SD.open("/system/settings.txt", FILE_WRITE);
+  File f = SD.open(SETTINGS_PATH, FILE_WRITE);
   // volume settings
   f.print(VOLUME_STRING);
   f.println(0.3);
@@ -24,7 +29,7 @@ void FileSystemManager::create_settings_file() {
   f.close();
 }
 void FileSystemManager::create_playlist_file() {
-  File f = SD.open("/system/playlist.txt", FILE_WRITE);
+  File f = SD.open(PLAYLIST_PATH, FILE_WRITE);
   f.print(PLAYLIST_ROOT_STRING);
   f.println("/songs/all");
   f.println(CURRENT_PLAYING_STRING);
@@ -32,10 +37,15 @@ void FileSystemManager::create_playlist_file() {
   f.close();
 }
 void FileSystemManager::create_song_file() {
-  File f = SD.open("/system/song.txt", FILE_WRITE);
+  File f = SD.open(SONG_PATH, FILE_WRITE);
   f.println(CURRENT_PLAYING_STRING);
 
   f.close();
+}
+File FileSystemManager::clear_file(const char *path) {
+  if (SD.exists(path)) {SD.remove(path);}
+
+  return SD.open(path, SETTINGS_PATH);
 }
 
 FileSystemManager::FileSystemManager() {
@@ -53,20 +63,20 @@ bool FileSystemManager::correct_sdcard_structure() {
   if (!SD.exists("/system")) {
     create_system_folder();
   }
-  else if (!SD.exists("/system/settings.txt")) {
+  else if (!SD.exists(SETTINGS_PATH)) {
     create_settings_file();
   }
-  else if (!SD.exists("/system/playlist.txt")) {
+  else if (!SD.exists(PLAYLIST_PATH)) {
     create_playlist_file();
   }
-  else if (!SD.exists("/system/song,txt")) {
+  else if (!SD.exists(SONG_PATH)) {
     create_song_file();
   }
   return true;
 }
 
 float FileSystemManager::load_saved_settings() {
-  File f = SD.open("/system/playlist.txt", FILE_WRITE);
+  File f = SD.open(PLAYLIST_PATH, FILE_WRITE);
   // read the header
   for (int i = 0; i < strlen(VOLUME_STRING); i++) {
     f.read();
@@ -82,13 +92,12 @@ float FileSystemManager::load_saved_settings() {
 }
 
 char* FileSystemManager::load_saved_playlist() {
-  File f = SD.open("/system/playlist.txt", FILE_WRITE);
-  // read the header first line
+  File f = SD.open(PLAYLIST_PATH, FILE_WRITE);
+  // read the header
   for (int i = 0; i < strlen(PLAYLIST_ROOT_STRING); i++) {
     f.read();
   }
 
-  //first line
   int buff_size1 = 127;
   auto playlist_root = new char[buff_size1];
   int cnt1 = 1;
@@ -115,18 +124,21 @@ char* FileSystemManager::load_saved_playlist() {
 }
 
 char *FileSystemManager::load_saved_song() {
-  File f = SD.open("/system/song.txt", FILE_WRITE);
-  // TODO check for null -> current_song = nullptr
-  // read the header second line
+  File f = SD.open(SONG_PATH, FILE_WRITE);
+  // read the header
   for (int i = 0; i < strlen(CURRENT_PLAYING_STRING); i++) {
     f.read();
   }
 
-  //second line
   int buff_size2 = 127;
   auto current_song = new char[buff_size2];
   int cnt2 = 1;
   int read2 = f.read();
+  if (read2 == '\n') {
+    f.close();
+    delete[] current_song;
+    return nullptr;
+  }
   current_song[0] = read2;
   while (read2 != static_cast<int>('\n')) {
     if (cnt2 < buff_size2 - 1) {
@@ -148,12 +160,24 @@ char *FileSystemManager::load_saved_song() {
 }
 
 void FileSystemManager::write_current_song(const char *current_song) {
+  File f = clear_file(SONG_PATH);
+
+  f.println(current_song);
+
+  f.close();
 
 }
-void FileSystemManager::write_playlist(const char *playlist_root,
-                                       const char *current_song) {
+void FileSystemManager::write_playlist(const char *playlist_root) {
+  File f = clear_file(PLAYLIST_PATH);
 
+  f.println(playlist_root);
+
+  f.close();
 }
 void FileSystemManager::write_settings(float volume) {
+  File f = clear_file(SETTINGS_PATH);
 
+  f.println(volume);
+
+  f.close();
 }
