a6496ee49ef27e768fc8d31925d5ad578b8d79fb
Vit Faltus <vit.faltus@proton.me>
2026-06-30 20:55:46 +0200

    Firmware: removed incorrect header file, moved correct header file to include dir

--- firmware/include/file_system_manager.hpp
+++ firmware/include/file_system_manager.hpp
@@ -1,26 +1,17 @@
-#pragma once
 #include "AudioFileSourceFS.h"
-
 class FileSystemManager {
-  File m_settings_file;
-  File m_playlist_file;
+  bool file_system_present = false;
 
-  static void create_system_folder();
-  static void create_settings_file();
-  static void create_playlist_file();
-  static void create_song_file();
-
-  static File clear_file(const char* path);
-
+  static bool filesys_setup();
 
   public:
   FileSystemManager();
-  static bool correct_sdcard_structure();
-  static float load_saved_settings(); // returns volume level for now
-  static char* load_saved_playlist();
-  static char* load_saved_song();
-  static void write_current_song(const char* current_song);
-  static void write_playlist(const char* playlist_root);
-  static void write_settings(float volume);
+
+  bool get_current_song_path(std::vector<char>& path) const;
+  bool set_current_song_path(const std::vector<char>& path) const;
+
+
+
+
 };
 
